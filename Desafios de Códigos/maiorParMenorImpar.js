a4fd335e8361af2d12707df3ad8ab3f1e93f89b5
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-04-18 16:55:24 -0300

    Desafio de Códigos - Ex2
    imprimir maior número par e menor número ímpar de uma lista

--- Desafios de Códigos/maiorParMenorImpar.js
+++ Desafios de Códigos/maiorParMenorImpar.js
@@ -15,3 +15,45 @@
 //             Menor número impar: 1
 
 const { gets, print } = require('./funcoesAuxiliaresEx2');
+
+const numero = gets();
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
+
+for (let i = 0; i < numero; i++) {
+    const numero = gets();
+
+    if (numero % 2 === 0) {
+        if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+            maiorNumeroPar = numero;
+        }
+
+    } else {
+        if (menorNumeroImpar === null || numero < menorNumeroImpar) {
+            menorNumeroImpar = numero;
+        }
+    }
+}
+
+/* RESOLUÇÃO INICIAL (NÃO SIMPLIFICADA)
+
+const { gets, print } = require('./funcoesAuxiliaresEx2');
+
+const numero = gets();
+let maiorNumeroPar = null;
+let menorNumeroImpar = null;
+
+for (let i = 0; i < numero; i++) {
+    const numero = gets();
+
+    if (numero % 2 === 0) {
+        if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+            maiorNumeroPar = numero;
+        }
+
+    } else {
+        if (menorNumeroImpar === null || numero < menorNumeroImpar) {
+            menorNumeroImpar = numero;
+        }
+    }
+}*/
