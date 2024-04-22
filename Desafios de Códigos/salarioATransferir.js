f6cf2638ab087a472437275a068e3d99e582ffe1
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-04-22 15:11:09 -0300

    Update salarioATransferir.js

--- Desafios de Códigos/salarioATransferir.js
+++ Desafios de Códigos/salarioATransferir.js
@@ -1,6 +1,6 @@
 // 3) Faça um programa que calcule e imprima o salário salário a ser transferido para um funcionário.
 // Para realizar o calculo receba o valor bruto do salário e o adicional dos benefícios.
-// O salário salário a ser transferido é calculado da seguinte maneira: 
+// O salário a ser transferido é calculado da seguinte maneira: 
    
 //    valor bruto do salário - percentual de imposto mediante a faixa salarial + adicional dos benefícios
 
@@ -19,3 +19,28 @@
 //             2050.00
 
 const { gets, print } = require('./funcoesAuxiliaresEx3');
+
+const valorSalarioBruto=gets();
+const valorBeneficios=gets();
+
+function calcularPorcentagem(valor,percentual){
+    return valor * (percentual/100);
+}
+
+function pegarAliquota(salario){
+
+    if (salario>=0 && salario<=1100.00) {
+        return 5;
+    } else if(salario>=1100.01 && salario <=2500){
+        return 10;
+    } else{
+        return 15;
+    }
+}
+const aliquotaImposto = pegarAliquota(valorSalarioBruto);
+
+const valorImposto=calcularPorcentagem(valorSalarioBruto, aliquotaImposto);
+
+const valorATransferir = valorSalarioBruto- valorImposto + valorBeneficios;
+
+print(valorATransferir);
