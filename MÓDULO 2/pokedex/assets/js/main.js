0aae7d966b2ba5c042af8f6c4ee2a022b82effe4
NeiJunio <neijunio50@gmail.com>
2024-06-27 22:04:24 -0300

    commit

---
+++ MÓDULO 2/pokedex/assets/js/main.js
@@ -0,0 +1,27 @@
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response) {
+
+        response
+            .json()
+            .then(function (responseBody){
+                console.log(responseBody)
+            })
+
+    })
+
+    .catch(function (error) {
+        console.error(error)
+    })
+
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
+
+    // try {
+    // } catch (error) { 
+    // } finally {
+    // }
